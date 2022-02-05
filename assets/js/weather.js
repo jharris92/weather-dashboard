cfb1281b4c1190f95bc038d46f030f61aa461fda
John Harris <john.harris7292@gmail.com>
2022-02-05 11:03:52 -0500

    added apiKey

---
+++ assets/js/weather.js
@@ -0,0 +1,8 @@
+var weatherEL = $("#weather-container");
+var historyEl = $("history");
+var apiKey = "aa630346e91a6441f826ab5f7a6be4a5";
+var searchHistory = [];
+
+function searchCity(event, city) {
+    
+}
